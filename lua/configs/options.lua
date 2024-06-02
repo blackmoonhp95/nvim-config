4ac167a77246e16c21683c30ff556b0d08d52f06
blackmoonhp95 <blackmoon8668@gmail.com>
2024-06-02 19:57:48 +0700

    Save configs

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -5,7 +5,8 @@ vim.opt.relativenumber = true
 -- Set width of a tab
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 2
+vim.opt.softtabstop = 4
+vim.o.expandtab = true
 
 -- Set theme
-vim.cmd("colorscheme nordfox")
+vim.cmd("colorscheme nightfox")
