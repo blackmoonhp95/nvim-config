bd9c5b3ffcdd357102968cc6d4ece26d36429499
blackmoonhp95 <blackmoon8668@gmail.com>
2024-10-05 05:02:45 +0700

    Disable lualine for NvimTree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,12 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 	config = function()
-		require('lualine').setup()
+		require('lualine').setup({
+            options = {
+                section_separators = '',
+                component_separators = '',
+                disabled_filetypes = {'NvimTree'}
+            }
+        })
 	end
 }
